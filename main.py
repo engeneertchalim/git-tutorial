3852c843a92fa8e4c1515af1a3c5207a7edfbced
tchalim <tchaliml@gmail.com>
2025-07-30 20:58:04 +0000

    new

--- main.py
+++ main.py
@@ -15,8 +15,24 @@ def demande_nom():
 
 def print_infos_user(name, age):
     print(f"your name is {name}")
+    print(f"Your age is {age} year(s)")
+
+    print("youpiiiiii")
+
+    print("use git commit -p")
+
+    print("")
+
+    print("commit with git commit -p")
+
+    print("git commit with -p without git add")
+
+    print("")
 
 
+nom = demande_nom()
 
+age = demande_info()
 
-print_infos_user('Tchalim')
+print_infos_user(nom, age)
+
