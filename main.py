2b37d10e52eda86879a5d9f0ebfc16726dc41ff8
tchalim <tchaliml@gmail.com>
2025-07-30 19:24:46 +0000

    deuxieme commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def print_infos_user(name):
+def print_infos_user(name, age):
     print(f"your name is {name}")
+    print(f"Your age is {age} year(s)")
 
 
 
 
-print_infos_user('Tchalim')
+print_infos_user('Tchalim', 20)
