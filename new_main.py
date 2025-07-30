e316217d576041c0b9195df872e3fe132da9b82d
tchalim <tchaliml@gmail.com>
2025-07-30 19:47:23 +0000

    commit 8

---
+++ new_main.py
@@ -0,0 +1,20 @@
+def print_infos_user(name, age):
+    print(f"your name is {name}")
+    print(f"Your age is {age} year(s)")
+
+    print("youpiiiiii")
+
+    print("use git commit -p")
+
+    print("")
+
+    print("commit with git commit -p")
+
+    print("git commit with -p without git add")
+
+    print("")
+
+
+
+
+print_infos_user('Tchalim', 20)
